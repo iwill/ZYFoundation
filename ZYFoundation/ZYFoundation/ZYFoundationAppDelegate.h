737c766bda32bbcfaf30b9895f8b2bbf2d11403b
ABeen <abeen_8298@msn.com>
2013-08-07 09:49:07 +0800

    create project

---
+++ ZYFoundation/ZYFoundation/ZYFoundationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZYFoundationAppDelegate.h
+//  ZYFoundation
+//
+//  Created by barfoo2 on 13-8-7.
+//  Copyright (c) 2013年 ZYProSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYFoundationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZYFoundation/ZYFoundation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZYFoundation
+//
+//  Created by barfoo2 on 13-8-7.
+//  Copyright (c) 2013年 ZYProSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZYFoundationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZYFoundationAppDelegate class]));
+    }
+}
